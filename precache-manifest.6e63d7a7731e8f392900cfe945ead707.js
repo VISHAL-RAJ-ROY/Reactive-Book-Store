c1f9ecfcef7b280cba654e0f576eb16c53c75b22
Vishal Raj <vishalrajroy2000@gmail.com>
2019-08-25 01:04:48 +0530

    Updates

---
+++ precache-manifest.6e63d7a7731e8f392900cfe945ead707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d702d015b1140fc069b4b4a78757e66e",
+    "url": "/Reactive-Book-Store/index.html"
+  },
+  {
+    "revision": "3553916b224e5b096c00",
+    "url": "/Reactive-Book-Store/static/css/main.f5a24af9.chunk.css"
+  },
+  {
+    "revision": "0b9b9f65abbf12f7fc65",
+    "url": "/Reactive-Book-Store/static/js/2.c4b73a1c.chunk.js"
+  },
+  {
+    "revision": "3553916b224e5b096c00",
+    "url": "/Reactive-Book-Store/static/js/main.d80e9024.chunk.js"
+  },
+  {
+    "revision": "386ceda3b43e49ac9f63",
+    "url": "/Reactive-Book-Store/static/js/runtime~main.474e741d.js"
+  }
+]);
